c2d6f22ef97bec9218d09d7dd8a05f3b869277a8
Fabian Soriano <fabo.soriano@gmail.com>
2022-10-27 19:32:05 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 
 function App() {
@@ -15,26 +18,25 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+        <Navbar variant="dark" bg='dark' >
+          <Container>
+            <Navbar.Brand>Monty's Mineral SPA</Navbar.Brand>
+            <Navbar.Text>
+              <Link to="/">Home</Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to="/about">About Us</Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to="/packages">Our Packages</Link>
+            </Navbar.Text>
+          </Container>
+        </Navbar>
 
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
+
 
         <div className="display">
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/packages" render={() => <Packages packages={packages}/>}  />
         </div>
